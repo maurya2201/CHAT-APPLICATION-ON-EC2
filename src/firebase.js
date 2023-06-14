1f586ece7e3347f33e72b7f6c6be5430a63f3773
MAURYA SONI <88384389+maurya2201@users.noreply.github.com>
2023-06-14 16:18:13 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,23 +2,16 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCPKEHmNaJjdQg_4uihJMU9RhaJJRse4R8",
-//   authDomain: "chat-b281c.firebaseapp.com",
-//   projectId: "chat-b281c",
-//   storageBucket: "chat-b281c.appspot.com",
-//   messagingSenderId: "833060935203",
-//   appId: "1:833060935203:web:a51d9c942071e0b2c98396"
-// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCq51SlxDLntXSz6L8r_bwJ_QQ6bdb3JdA",
-    authDomain: "chat3-aaee1.firebaseapp.com",
-    projectId: "chat3-aaee1",
-    storageBucket: "chat3-aaee1.appspot.com",
-    messagingSenderId: "762277979454",
-    appId: "1:762277979454:web:707c4a913a04dcbac8f86f"
-  };
-// Initialize Firebase
+  apiKey: "AIzaSyCwawCGgUGGpQbANR5Af-Fud0lM-bjdcE4",
+  authDomain: "mychatapp-fbb0a.firebaseapp.com",
+  projectId: "mychatapp-fbb0a",
+  storageBucket: "mychatapp-fbb0a.appspot.com",
+  messagingSenderId: "692108416944",
+  appId: "1:692108416944:web:48b173d38a80824d406875"
+};
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();

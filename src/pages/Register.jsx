e6b5ca77591289573f3b38bf68bc3108385e2ad1
MAURYA SONI <88384389+maurya2201@users.noreply.github.com>
2023-06-16 13:33:50 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -52,7 +52,7 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Display Name" />
           <input type="email" placeholder="Email" />
-          <input type="text" placeholder="Password" />
+          <input type="password" placeholder="Password" />
           <input style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img alt="chat" height={20} width={32} src={Camera} />
